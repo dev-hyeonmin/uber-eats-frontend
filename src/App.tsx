6c739e84af0b39256b50223c5f9d979e6118ecd9
ROHHYEONMIN <roh._.hm@RohHyeonMinui-MacBookPro.local>
2022-05-25 20:37:33 +0900

    Add Login Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "./apollo";
+import { LoggedInRouter } from "./routers/logged-in-router";
 import { LoggedOutRouter } from "./routers/logged-out-router";
 
 
 function App() {
-  return (
-    <div>
-      <LoggedOutRouter/>
-    </div>
-  );
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
